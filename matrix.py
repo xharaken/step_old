a05f27ac8f87b4d9d313d1435228984ea684ab81
haraken <haraken@haraken2.roam.corp.google.com>
2018-06-06 09:23:03 +0900

    temp

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import numpy, sys, time
 
 if (len(sys.argv) != 2):
-    print "usage: python %s N" % sys.argv[0]
+    print("usage: python %s N" % sys.argv[0])
     quit()
 
 n = int(sys.argv[1])
@@ -10,27 +10,28 @@ b = numpy.zeros((n, n)) # Matrix B
 c = numpy.zeros((n, n)) # Matrix C
 
 # Initialize the matrices to some values.
-for i in xrange(n):
-    for j in xrange(n):
+for i in range(n):
+    for j in range(n):
         a[i, j] = i * n + j
         b[i, j] = j * n + i
         c[i, j] = 0
 
 begin = time.time()
 
-######################################
-# Write code to calculate C = A * B. #
-######################################
+######################################################
+# Write code to calculate C = A * B                  #
+# (without using numpy librarlies e.g., numpy.dot()) #
+######################################################
 
 end = time.time()
-print "time: %.6f sec" % (end - begin)
+print("time: %.6f sec" % (end - begin))
 
 # Print C for debugging. Comment out the print before measuring the execution time.
 total = 0
-for i in xrange(n):
-    for j in xrange(n):
+for i in range(n):
+    for j in range(n):
         # print c[i, j]
         total += c[i, j]
 # Print out the sum of all values in C.
 # This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
-print "sum: %.6f" % total
+print("sum: %.6f" % total)
